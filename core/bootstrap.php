1b40dcf81beb33610d3064b9f30fb7a7c0d12f8e
ManniCoon <63167163+Mannicoon@users.noreply.github.com>
2023-01-16 17:20:35 +0200

    Update 0.1

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -7,7 +7,7 @@ require_once('core/Router.php');
 require_once('core/database/connection.php');
 require_once('core/database/QueryBuilder.php');
 
-
-return new QueryBuilder(
-    Connection::make($config['database'])
-);
+// NB! UNCOMMENT WHEN USING DATABASE
+// return new QueryBuilder(
+//     Connection::make($config['database'])
+// );
