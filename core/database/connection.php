e31b25ae78a4776e64a1b919c820c4a560d9cd25
ManniCoon <63167163+Mannicoon@users.noreply.github.com>
2023-01-19 22:59:34 +0200

    Update connection.php

--- core/database/connection.php
+++ core/database/connection.php
@@ -2,8 +2,6 @@
 
 class Connection {
     public static function make($config) {
-        
-
         try {
             return new PDO(
                 "mysql:host={$config['host']};dbname={$config['db']};charset={$config['charset']}",
